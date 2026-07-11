30f11f94c45f90683f0dc3e81ec1aa17bba3e240
fys <fengys1996@gmail.com>
2026-07-11 13:41:08 +0800

    init project

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: {
+    nixosConfigurations.desktop =
+      nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/destop/configuration.nix
+          home-manager.nixosModules.default
+          {
+            home-manager.users.fys = import ./home.nix;
+          }
+        ];
+      };
+  };
+}
---
+++ home/fys/home.nix
@@ -0,0 +1,55 @@
+{ pkgs, ... }:
+
+{
+  home.username = "fys";
+  home.homeDirectory = "/home/fys";
+
+  home.stateVersion = "25.05";
+
+  programs.neovim.enable = true;
+  programs.neovim.withRuby = false;
+  programs.neovim.withPython3 = false;
+  
+  xdg.configFile."nvim".source = ./nvim-config;
+
+  wayland.windowManager.sway.enable = true;
+  xdg.configFile."sway".source = ./dot/sway;
+  
+  xdg.configFile."./xkb/symbols/us_minila_r".source = ./dot/xkb/us_minila_r;
+
+  programs.fish.enable = true;
+  xdg.configFile."fish".source = ./dot/fish;
+
+  programs.alacritty.enable = true;
+  xdg.configFile."alacritty".source = ./dot/alacritty;
+
+  xdg.configFile."wofi".source = ./dot/wofi;
+
+  programs.git = {
+    enable = true;
+
+    settings = {
+      user = {
+        name = "fys";
+        email = "fengys1996@gmail.com";
+      };
+    };
+  };
+  
+  home.packages = with pkgs; [
+    mold
+    clang
+    protobuf
+    taplo
+    curl
+    ripgrep
+    fd
+    bat
+    eza
+    jq
+    tree
+    htop
+    btop
+    wofi
+  ];
+}
